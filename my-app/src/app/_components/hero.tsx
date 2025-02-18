78e9359f0c3d4f763459f6b5eb5d040e014ac3c6
Guilherme Oliveira <glrm07928@gmail.com>
2025-02-18 17:56:38 -0300

    .

--- my-app/src/app/_components/hero.tsx
+++ my-app/src/app/_components/hero.tsx
@@ -2,23 +2,19 @@
 
 export function Hero(){
     return(
-       <section className="bg-[#2C520C] relative overflow-hidden text-zinc-50">
-            <div className="container mx-auto py-16 px-4 relative">
+       <section className="sm:bg-[url('/bg.jpeg')]  bg-[url('/bg-sd.jpeg')] relative overflow-hidden text-zinc-50 bg-no-repeat bg-cover bg-center">
+            <div className="container mx-auto py-16 md:pb-0 px-4 relative">
                 <article className="grid grid-cols-1 lg:grid-cols-2 gap-8 ">
-                    <div className="space-y-6">
+                    <div className="space-y-6 flex justify-center flex-col p-10">
                         <h1 className="text-3xl md:text-4xl font-bold leading-10">
                         Um local ideal para
                         desfrutar da terceira idade e aproveitar ao
                         máximo a vida
                         </h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi praesentium officia modi qui consequuntur a corrupti aspernatur laudantium perferendis veritatis?</p>
                         <div className="flex">
                             <a className="bg-green-500 px-5 py-2 rounded font-semibold flex justify-center items-center" href="#">Entre em contato</a>
                         </div>
-                    </div>
-                    <div className="hidden md:block">
-                        <h1>
-                            foto 
-                        </h1>
                     </div>
                 </article>
             </div>
