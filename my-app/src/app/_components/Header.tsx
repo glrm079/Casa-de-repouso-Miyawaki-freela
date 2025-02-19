086233df6d29887defa02aca0740778b04c7b1af
Guilherme Oliveira <glrm07928@gmail.com>
2025-02-19 18:16:38 -0300

    header

---
+++ my-app/src/app/_components/Header.tsx
@@ -0,0 +1,22 @@
+
+export function Header(){
+    return(
+        <section className="overflow-hidden  bg-[#2c520c]">
+            <div className="container mx-auto relative">
+                <article className="flex flex-row justify-between items-center px-16 py-3 text-zinc-50 ">
+                    <div>
+                        <img src="" alt="Logo" />
+                    </div>
+                    <nav className="flex-row items-center gap-4 hidden md:flex">
+                        <a href="#">Home</a>
+                        <a href="#">Home</a>
+                        <a href="#">Home</a>
+                        <a href="#">Home</a>
+                        <a className="bg-green-500 px-5 py-2 rounded font-semibold flex justify-center items-center" href="#">Entre em contato</a>
+                    </nav>
+
+                </article>
+            </div>
+        </section>
+    )
+}
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,7 +1,13 @@
-import { Hero } from "./_components/hero";
+
+import { Hero } from "./_components/Hero";
+import {Header} from "./_components/Header"
 
 export default function Home() {
   return (
-    <Hero/>
+    <>
+      <Header/>
+      <Hero/>
+    </>
+    
   );
 }
