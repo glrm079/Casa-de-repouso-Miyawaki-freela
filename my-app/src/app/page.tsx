3fd50b9e3b1bc630374b85423a2bc6ae79bd4534
Guilherme Oliveira <glrm07928@gmail.com>
2025-03-03 15:17:30 -0300

    .

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,5 +1,5 @@
 import {Header} from "./_components/Header"
-import { Hero } from "./_components/Hero";
+import {Hero} from "./_components/Hero";
 import {About} from"./_components/About";
 import {Diferenciais} from"./_components/Diferenciais";
 import {Equipe} from"./_components/Equipe";
